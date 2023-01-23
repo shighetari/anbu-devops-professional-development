d58c66cb44880c4cc79239d85221f8646f651c40
Francisco <shighetari@gmail.com>
2023-01-22 22:33:29 -0500

    configured dockerfile and script files for windows, mac os, and linux with .shell and .batch files respectively.

--- Karuma/frontend/vulpix-UI/.run_ui.sh
+++ Karuma/frontend/vulpix-UI/.run_ui.sh
@@ -1,4 +1,13 @@
-cd ..\..
-cd ui
-docker build -t my-ui-app .
-docker run -it -p 4200:4200 my-ui-app
+#!/bin/bash
+
+# Stop any running containers
+docker container stop $(docker container ls -aq)
+
+# Remove any existing containers
+docker container rm $(docker container ls -aq)
+
+# Build the container
+docker build -t app .
+
+# Run the container
+docker run -it --rm -p 4200:4200 app
