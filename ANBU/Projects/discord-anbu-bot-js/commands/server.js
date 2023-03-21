40b78ba407b1d15de6442163a212c33ea4783719
Francisco <shighetari@gmail.com>
2023-03-21 02:35:04 -0400

    finally fixed server command crashing the node server. Always pay attention to versions and updated documentation...

--- ANBU/Projects/discord-anbu-bot-js/commands/server.js
+++ ANBU/Projects/discord-anbu-bot-js/commands/server.js
@@ -1,7 +1,36 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'server',
     description: 'Display server information',
     async execute(interaction) {
-      interaction.reply({ content: `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`, ephemeral: true });
-    },
-  };
+        try {
+            const guild = interaction.guild;
+            const createdAt = guild.createdAt.toDateString();
+            const owner = guild.owner ? guild.owner.user.tag : 'N/A';
+            const region = guild.region || 'N/A'; // Add default value in case region is not available
+            const icon = guild.iconURL({ dynamic: true }) || 'N/A';
+
+            const embed = new MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle(guild.name)
+                .setDescription(`Here is some information about the server ${guild.name}:`)
+                .addFields(
+                    { name: 'Created at', value: createdAt, inline: true },
+                    { name: 'Server owner', value: owner, inline: true },
+                    { name: 'Region', value: region, inline: true }
+                )
+                .setThumbnail(icon)
+                .setTimestamp()
+                .setFooter('Requested by ' + interaction.user.tag);
+
+            await interaction.reply({ embeds: [embed], ephemeral: true });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({
+                content: 'There was an error while executing this command!',
+                ephemeral: true
+            });
+        }
+    }
+};
